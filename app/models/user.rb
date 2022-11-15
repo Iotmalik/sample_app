cd91503158f266c6fac8680c44eeba157f044c9f
Alexey <kriz05@mail.ru>
2022-11-15 19:38:35 +0300

    Refactoring and add tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,8 +66,7 @@ class User < ApplicationRecord
 
   # Активирует учетную запись. 
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now) 
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
   # Посылает письмо со ссылкой на страницу активации. 
   def send_activation_email
@@ -77,8 +76,7 @@ class User < ApplicationRecord
   # Устанавливает атрибуты для сброса пароля. 
   def create_reset_digest
     self.reset_token = User.new_token 
-    update_attribute(:reset_digest, User.digest(reset_token)) 
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   # Посылает письмо со ссылкой на форму ввода нового пароля. 
